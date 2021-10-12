961d8370410bf0be50fba6fa55419f958ebec2ca
08-AaronTan <91874857+08-AaronTan@users.noreply.github.com>
2021-10-12 20:58:41 +0800

    Adding point to coin collected and player will go game lose once the touches water

---
+++ Assets/Script/CoinScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CoinScript : MonoBehaviour
+{
+    public int coinCounter = 0;
+    public Text coinText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coinCounter = 0; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        coinText.text = "Coin: " + coinCounter; 
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag.Equals("Coin"))
+        {
+            coinCounter += 10;
+
+            Destroy(other.gameObject);
+        }
+
+        if(other.gameObject.tag.Equals("water"))
+        {
+            SceneManager.LoadScene("GameLose");
+        }
+    }
+
+
+}
