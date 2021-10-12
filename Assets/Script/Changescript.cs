8c450c40fd6eda62401a8732daef7dd66e10aaf9
08-AaronTan <91874857+08-AaronTan@users.noreply.github.com>
2021-10-12 22:45:55 +0800

    built particle and add sound effect

--- Assets/Script/Changescript.cs
+++ Assets/Script/Changescript.cs
@@ -20,5 +20,6 @@ public class Changescript : MonoBehaviour
     public void RestartButton()
     {
         SceneManager.LoadScene("GameScene");
+
     }
 }
--- Assets/Script/CoinScript.cs
+++ Assets/Script/CoinScript.cs
@@ -3,21 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class CoinScript : MonoBehaviour
 {
     public int coinCounter = 0;
     public Text coinText;
 
+    public float totalcoins;
+    public float timeleft;
+    public int timeRemaining;
+
+    public Text TimerText;
+
+    private float TimerValue;
+
+    public ParticleSystem CoinPop;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        coinCounter = 0; 
+        CoinPop.Stop();
+
+        coinCounter = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        timeleft -= Time.deltaTime;
+
+        timeRemaining = Mathf.FloorToInt(timeleft % 60);
+
+        TimerText.text = "Timer: " + timeRemaining.ToString();
+
+        if(coinCounter == totalcoins)
+        {
+            if(timeleft <= TimerValue)
+            {
+                SceneManager.LoadScene("GameWin");
+            }
+        }
+        else if(timeleft <= 0)
+        {
+            SceneManager.LoadScene("GameLose");
+
+        }
+
+        GameObject.Find("FPSController").GetComponent<FirstPersonController>().m_MouseLook.m_cursorIsLocked = false;
+
+
         coinText.text = "Coin: " + coinCounter; 
     }
 
@@ -27,12 +64,17 @@ public class CoinScript : MonoBehaviour
         {
             coinCounter += 10;
 
+            CoinPop.Play();
+
             Destroy(other.gameObject);
         }
 
         if(other.gameObject.tag.Equals("water"))
         {
             SceneManager.LoadScene("GameLose");
+
+            GameObject.Find("FPSController").GetComponent<FirstPersonController>().m_MouseLook.m_cursorIsLocked = false;
+
         }
     }
 
